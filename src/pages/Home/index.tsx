50bfa8bd5001dfa39b032981d8d8208673027120
artuguen28 <arthur.uguen28@gmail.com>
2023-08-07 09:27:46 -0400

    Front-end Update

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,36 @@
 import "./styles.css"
+import SCI from "../../assets/SCI.svg"
+import { useNavigate } from "react-router-dom";
 
 export default function Home() {
 
+	const navigate = useNavigate();
+
 	return (
-		<>
-			<div className="center-panel">
-				<p>Hello, World!</p>
+		<div className="container">
+			<div className="top-bar">
+				<img className="sci-logo" src={SCI} alt="SCI Logo"/>
+				<h1>Wellcome to Scientiary</h1>
+				<button onClick={() => navigate("/")} className="logout-button" >LogOut</button>
 			</div>
-		</>
+			<div className="body-screen">
+				<div className="side-bar">
+					<h1>Categorias</h1>
+					<div className="options">
+						<p>Título</p>
+						<p>Autor</p>
+						<p>Gênero</p>
+					</div>
+				</div>
+				<div className="sci-main">
+					<div className="search">
+						<h1>Pesquisa</h1>
+						<input
+							type="text"/>
+					</div>
+				</div>
+			</div>
+
+		</div>
 	)
 }
--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,22 +1,24 @@
-// import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "./styles.css"
 import SCI from "../../assets/SCI.svg"
 import MetaMask_Fox from "../../assets/MetaMask_Fox.svg"
 
 function MainPage() {
 
+	const navigate = useNavigate();
+
     return (
 		<div className="panel-container">
-			<div className="right-panel">
+			<div className="left-panel">
 				<img className="sci-logo" src={SCI} alt="SCI Logo"/>
 				<h2><span className='notbold'>Bem-vindo ao </span>Scientiary</h2>
 				<p>Já possui uma conta MetaMask? Acesse agora! </p>
 				<div className="button-bar">
 					<img className="meta-logo" src={MetaMask_Fox} alt="Meta Logo"/>
-					<button className="enter-button" >ENTRAR</button>
+					<button onClick={() => navigate("/home")} className="enter-button" >ENTRAR</button>
 				</div>
 			</div>
-			<div className="left-panel">
+			<div className="right-panel">
 				<h2>Como se cadastrar?</h2>
 				<p>A biblioteca científica pode ser acessada sincronizando sua sessão com uma carteira Ethereum. </p>
 				<p>Recomendamos a utilização da carteira Metamask.</p>
